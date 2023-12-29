b3d1d3fa1958b675cecbd0ead1e9a81d7be54421
jkwon-dev <kwonejk@gmail.com>
2023-12-30 03:03:56 +0900

    Displaying data in a list 2

--- CardView.swift
+++ CardView.swift
@@ -8,11 +8,32 @@
 import SwiftUI
 
 struct CardView: View {
+    let scrum: DailyScrum
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading) {
+            Text(scrum.title)
+                .font(.headline)
+                .accessibilityAddTraits(.isHeader)
+            Spacer()
+            HStack{
+                Label("\(scrum.attendees.count)", systemImage: "person.3")
+                    .accessibilityLabel("\(scrum.attendees.count) attendees")
+                Spacer()
+                Label("\(scrum.lengthInMinutes)", systemImage: "clock")
+                    .accessibilityLabel("\(scrum.lengthInMinutes) minute meeting")
+                    .labelStyle(.trailingIcon)
+            }
+            .font(.caption)
+        }
+        .padding()
+        .foregroundStyle(scrum.theme.accentColor)
     }
 }
 
 #Preview {
-    CardView()
+    
+    CardView(scrum: DailyScrum.sampleData[0])
+        .frame(width: 400, height:60)
+        //.previewLayout(.fixed(width: 400, height: 60))
+
 }
