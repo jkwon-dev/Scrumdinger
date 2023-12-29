408deac17a5dcfa9f420c26afded1f551b3fad40
jkwon-dev <kwonejk@gmail.com>
2023-12-30 03:01:55 +0900

    Displaying  data in a list

---
+++ Scrumdinger/ScrumsView.swift
@@ -0,0 +1,18 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by kwon eunji on 12/30/23.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScrumsView()
+}
